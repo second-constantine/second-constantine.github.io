5baa9263ec872f8756dbb8b7fb517de1f8b91230
second_constantine <cs@next-way.by>
2020-04-12 21:51:53 +0300

    merge with itcompresoor. Some refactor and style.

--- js/app.js
+++ js/app.js
@@ -2,16 +2,62 @@ var app = angular.module("core-app", []);
 app.controller("CoreController",
     ["$scope", "$http", "$sce", "$filter", function ($scope, $http, $sce, $filter) {
 
+        $scope.allPosts = []
+        $scope.newPosts = []
+        $scope.posts = []
+
         $http.get("data/category.json").success(function (response) {
             $scope.categories = response
-        });
-        $http.get("data/pinned.json").success(function (response) {
-            $scope.pinned = response
+            $http.get("data/pinned.json").success(function (response) {
+                $scope.pinned = response
+                $http.get("data/posts.json").success(function (response) {
+                    $scope.allPosts = response
+                    coreFunction()
+                });
+            });
         });
 
-        $scope.renderHtml = function (htmlCode) {
-            return $sce.trustAsHtml(htmlCode);
-        };
+        var getPostById = function (id) {
+            var result = $scope.allPosts.filter(function (post) {
+                return post.id == id;
+            })[0]
+            return result;
+        }
+
+        $scope.homeRedirect = function () {
+            $scope.isTarget = false;
+            $scope.newPosts = $scope.allPosts.slice(0, 2);
+            $scope.posts = $scope.allPosts.slice(2);
+            document.title = "Constantine's blog";
+            document.location = document.location;
+            document.location.hash = "";
+            console.log("homeRedirect")
+        }
+
+        $scope.clickPost = function (id) {
+            var post = getPostById(id)
+            $scope.isTarget = true;
+            $scope.postUrl = "data/html/" + post.id + ".html";
+            $scope.target = id;
+            $scope.targetPost = post;
+            document.title = post.description;
+            document.location.hash = "?target=" + id;
+            console.log("clickPost=" + id)
+        }
+
+        $scope.clickCategory = function (category) {
+            let filteredPosts = $scope.allPosts.filter(post => post.category.name == category)
+            document.title = "Constantine's blog"
+            if (filteredPosts.length > 2) {
+                $scope.newPosts = filteredPosts.slice(0, 2)
+                $scope.posts = filteredPosts.slice(2)
+            } else {
+                $scope.newPosts = filteredPosts
+                $scope.posts = []
+            }
+            document.location.hash = "?category=" + category;
+            console.log("clickCategory=" + category)
+        }
 
         var getQueryParams = function (qs) {
             qs = qs.split("+").join(" ");
@@ -23,49 +69,34 @@ app.controller("CoreController",
             return params;
         }
 
-        var searchAny = function () {
-            var urlParams = getQueryParams(document.location.hash);
-            $scope.search = urlParams["s"];
-            $scope.target = urlParams["target"]
-        }
-
         $scope.changeSearch = function (search) {
             $scope.search = search;
+            document.location.hash = "?s=" + search;
+            console.log("search=" + search)
+        }
+
+        var coreFunction = function () {
+            var params = getQueryParams(document.location.hash);
+            console.log(params);
+            $scope.isTarget = false;
+            var category = params["category"];
+            if (category !== undefined) {
+                $scope.clickCategory(category);
+            } else {
+                var postId = params["target"];
+                if (postId !== undefined) {
+                    $scope.clickPost(postId);
+                } else {
+                    if (params.s === undefined) {
+                        $scope.homeRedirect();
+                    }
+                }
+            }
         }
 
         window.onhashchange = function () {
-            var urlParams = getQueryParams(document.location.hash);
-            var postId = urlParams["post_id"]
-            console.log(urlParams)
-            $http.get("data/content.json").success(function (response) {
-                $scope.isPostMode = false;
-                $scope.newPosts = response.newPosts
-                $scope.recentPosts = response.recentPosts
-                var category = urlParams["category"]
-                if (category !== undefined) {
-                    $scope.newPosts = $scope.newPosts.filter(post => post.category.name == category)
-                    $scope.recentPosts = $scope.recentPosts.filter(post => post.category.name == category)
-                }
-                if (postId !== undefined) {
-                    let findInNewPost = $scope.newPosts.filter(post => post.id == postId)
-                    let post
-                    if (findInNewPost.length == 0) {
-                        let findInRecentPosts = $scope.recentPosts.filter(post => post.id == postId)
-                        if (findInRecentPosts == 0) {
-                            post = null
-                        } else {
-                            post = findInRecentPosts[0]
-                        }
-                    } else {
-                        post = findInNewPost[0]
-                    }
-                    if (post != null) {
-                        document.title = post.description
-                        $scope.isPostMode = true;
-                        $scope.targetPost = post;
-                    }
-                }
-            });
+            this.console.log("onhashchange");
+            coreFunction();
+            $scope.$apply();
         }
-        window.onhashchange();
     }]);
